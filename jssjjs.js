9048b797b58c5d7d8693542ec84a89c49ca093e3
Verezub Olena <ussanel@ukr.net>
2023-04-05 15:12:59 +0300

    done

--- jssjjs.js
+++ jssjjs.js
@@ -20,77 +20,82 @@
 //     return index<it ? pronto(arr,++index,sum):console.log('done')}
 //     pronto(arr)
 
-// // Завдання 2  
+// // // Завдання 2  :((
 
-let it
-// function deepCount(arg){
-//     for(let key in arg){
-//         if (Array.isArray(arg[key])){      
-//         }
-//         else(
-//             deepCount(arg[key]) 
-//         )
-//     }
-// }
-// console.log(deepCount([]))
-// 0
-// console.log(deepCount[1, 2, 3]);
-// 3
-// console.log(deepCount([x, y, [z]]));
-// 4
-console.log(deepCount([1, 2[3, 4[5]]]));
-// 7
-// console.log(deepCount([
-//     [
-//         [
-//             []
-//         ]
-//     ]
-// ]));
-// 3
-
-
-// Завдання 3 
-// const employees = {
-//     development: {
-//         backend: [{name: 'Mike', salary: 2000}, {name: 'Nikola', salary: 2500}],
-//         frontend: [{name: 'Artem', salary: 3000}, {name: 'Alex', salary: 2700}],
-//     },
-//     sales: {
-//         marketing: {
-//             internet_marketers: [{name: 'Nina', salary: 1000}, {name: 'Olena', salary: 1300}],
-//             promotion: [{name: 'Oleg', salary: 1400}, {name: 'Masha', salary: 1700}],
-//         },
-//         sellers:  [{name: 'Max', salary: 900}, {name: 'Donald', salary: 700}, {name: 'Joe', salary: 1100}]
-//     },
-//     designer: [{name: 'Kate', salary: 1800}]
-// }
-// function countSalary (){
-//     let sum = 0;
-//     return function sumSallary(obj){
-//         for(let key in obj){
-//             if (Array.isArray(obj[key])){
-//                 obj[key].forEach(employee => sum += employee.salary);
-//             } else {
-//                 sumSallary(obj[key]);
+// function deepCount(arg) {
+//     let it = arg.length
+//     for (let key in arg) {
+//         if (Array.isArray(arg[key])) {
+//                 it += deepCount(arg[key])
 //             }
+           
 //         }
-//         return sum;
-//     }
+//         return it;
 //  }
-//  let paySalary = countSalary();
-//  console.log(paySalary(employees))
+// let user= [1,2,3]
+// console.log(deepCount(user))
+// const fake = [1,2,[3]]
+// console.log (deepCount(fake))
 
-// sum = 4
-// console.log(sum);
+//     console.log(deepCount([]))
+//     // 0
+//     console.log(deepCount[1, 2, 3]);
+//     // 3
+//      console.log(deepCount([x, y, [z]]));
+//     // 4
+//     console.log(deepCount([1, 2[3, 4[5]]]));
+//     //  7
+//     console.log(deepCount([
+//         [
+//             [
+//                 []
+//             ]
+//         ]
+//     ]));
+//     //3
 
-// Завдання 4:)
-// function mx(arg){
-// return  Math.max(...arg)
-// }
-// let arr1 = [2, 15 ,7, 3 ];
-// console.log(mx(arr1))
-// let arr2 = [9, 3, 17, 12, 4, 8 ];
-// console.log(mx(arr2))
-// let arr3 = [6, 11, 16, 1,5, 11];
-// console.log(mx(arr3))
+
+    // Завдання 3 
+    // const employees = {
+    //     development: {
+    //         backend: [{name: 'Mike', salary: 2000}, {name: 'Nikola', salary: 2500}],
+    //         frontend: [{name: 'Artem', salary: 3000}, {name: 'Alex', salary: 2700}],
+    //     },
+    //     sales: {
+    //         marketing: {
+    //             internet_marketers: [{name: 'Nina', salary: 1000}, {name: 'Olena', salary: 1300}],
+    //             promotion: [{name: 'Oleg', salary: 1400}, {name: 'Masha', salary: 1700}],
+    //         },
+    //         sellers:  [{name: 'Max', salary: 900}, {name: 'Donald', salary: 700}, {name: 'Joe', salary: 1100}]
+    //     },
+    //     designer: [{name: 'Kate', salary: 1800}]
+    // }
+    // function countSalary (){
+    //     let sum = 0;
+    //     return function sumSallary(obj){
+    //         for(let key in obj){
+    //             if (Array.isArray(obj[key])){
+    //                 obj[key].forEach(employee => sum += employee.salary);
+    //             } else {
+    //                 sumSallary(obj[key]);
+    //             }
+    //         }
+    //         return sum;
+    //     }
+    //  }
+    //  let paySalary = countSalary();
+    //  console.log(paySalary(employees))
+
+    // sum = 4
+    // console.log(sum);
+
+    // Завдання 4:)
+    // function mx(arg){
+    // return  Math.max(...arg)
+    // }
+    // let arr1 = [2, 15 ,7, 3 ];
+    // console.log(mx(arr1))
+    // let arr2 = [9, 3, 17, 12, 4, 8 ];
+    // console.log(mx(arr2))
+    // let arr3 = [6, 11, 16, 1,5, 11];
+    // console.log(mx(arr3))
